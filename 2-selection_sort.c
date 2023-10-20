d13c35f72a574d87774b14504e26a7d731dae022
AnsaYong <ansahmbomyong@gmail.com>
2023-10-20 21:43:43 +0200

    updated checks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,34 +16,33 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	else
+
+	/* loop through the entire array */
+	for (i = 0; i < size - 1; i++)
 	{
-		/* loop through the entire array */
-		for (i = 0; i < size - 1; i++)
+		min = i;	/* assume the current index has the least value */
+
+		/* find the smallest number between array[i] and array[n - 1] */
+		for (j = i + 1; j < size; j++)
 		{
-			min = i;	/* assume the current index has the least value */
-
-			/* find the smallest number between array[i] and array[n - 1] */
-			for (j = i + 1; j < size; j++)
+			/* smallest number by comparing the 2 successive numbers */
+			if (array[j] < array[min])
 			{
-				/* smallest number by comparing the 2 successive numbers */
-				if (array[j] < array[min])
-				{
-					/* update index with smallest number accordingly */
-					min = j;
-				}
+				/* update index with smallest number accordingly */
+				min = j;
 			}
-			/* Swap smallest number with array[i] */
-			if (min != i)
-			{
-				/* swap value at index i with value at the new index */
-				temp = array[min];
-				array[min] = array[i];
-				array[i] = temp;
-			}
-
-			/* Print the array after each swap */
-			print_array(array, size);
 		}
+		/* Swap smallest number with array[i] */
+		if (min != i)
+		{
+			/* swap value at index i with value at the new index */
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+		}
+
+		/* Print the array after each swap */
+		print_array(array, size);
 	}
+
 }

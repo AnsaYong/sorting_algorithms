d38b4d9ae3c2270251381d40dfd8dbf76af36d34
AnsaYong <ansahmbomyong@gmail.com>
2023-10-23 20:22:21 +0200

    updating betty requirements

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,8 +7,7 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int max_value = 0, index = 1;
-	int *count_array = NULL, *temp_array = NULL;
+	int max_value = 0, index = 1, *count_array = NULL, *temp_array = NULL;
 
 	if (!array || size < 2)
 		return;
@@ -20,27 +19,22 @@ void counting_sort(int *array, size_t size)
 			max_value = array[index];
 		index++;
 	}
-
 	temp_array = malloc(sizeof(int) * size);
 	if (temp_array == NULL)
 		return;
-
 	count_array = malloc(sizeof(int) * max_value + 1);
-    if (count_array == NULL)
+	if (count_array == NULL)
 	{
 		free(temp_array);
 		return;
 	}
-
 	for (index = 0; index <= max_value; ++index)
 		count_array[index] = 0;
 	for (index = 0; index < (int) size; ++index)
 		count_array[array[index]]++;
 	for (index = 1; index <= max_value; ++index)
 		count_array[index] = count_array[index - 1] + count_array[index];
-
-    print_array(count_array, max_value + 1);
-
+	print_array(count_array, max_value + 1);
 	for (index = 0; index < (int) size; ++index)
 	{
 		temp_array[count_array[array[index]] - 1] = array[index];
@@ -48,7 +42,6 @@ void counting_sort(int *array, size_t size)
 	}
 	for (index = 0; index < (int) size; ++index)
 		array[index] = temp_array[index];
-
 	free(count_array);
 	free(temp_array);
 	count_array = temp_array = NULL;
